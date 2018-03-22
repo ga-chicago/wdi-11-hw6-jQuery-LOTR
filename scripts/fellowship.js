fdb020317ac75cffe8418a351b27ebd54374984d
Reuben Ayres <ra@sent.com>
2018-03-22 15:30:19 -0500

    Added exercise

---
+++ scripts/fellowship.js
@@ -0,0 +1,93 @@
+console.log("Linked.");
+
+// Dramatis Personae
+var hobbits = [
+  'Frodo Baggins',
+  'Samwise \'Sam\' Gamgee',
+  'Meriadoc \'Merry\' Brandybuck',
+  'Peregrin \'Pippin\' Took'
+];
+
+var buddies = [
+  'Gandalf the Grey',
+  'Legolas',
+  'Gimli',
+  'Strider',
+  'Boromir'
+];
+
+var lands = ['The Shire', 'Rivendell', 'Mordor'];
+var body = document.body;
+var section = '<section></section>';
+
+function makeMiddleEarth() {
+  // your answers here
+}
+
+makeMiddleEarth();
+
+function makeHobbits(){
+  // your answers here
+}
+
+makeHobbits();
+
+function keepItSecretKeepItSafe(){
+  // your answers here
+}
+
+keepItSecretKeepItSafe();
+
+function makeBuddies(){
+  // your answers here
+}
+
+makeBuddies();
+
+function beautifulStranger(){
+  // your answers here
+}
+
+beautifulStranger();
+
+function leaveTheShire(){
+  // your answers here
+}
+
+leaveTheShire();
+
+function forgeTheFellowship() {
+  // your answers here
+}
+
+forgeTheFellowship();
+
+function theBalrog(){
+  // your answers here
+}
+
+theBalrog();
+
+function hornOfGondor() {
+  // your answers here
+}
+
+hornOfGondor();
+
+function itsDangerousToGoAlone() {
+  // your answers here
+}
+
+itsDangerousToGoAlone();
+
+function weWantsIt() {
+  // your answers here
+}
+
+weWantsIt();
+
+function thereAndBackAgain(){
+  // your answers here
+}
+
+thereAndBackAgain();
