e45934aa332d31548b99e4520dc626489630e7c2
agraham <agraham0925@gmail.com>
2018-03-22 16:29:24 -0500

    completed part one

--- scripts/fellowship.js
+++ scripts/fellowship.js
@@ -21,13 +21,27 @@ var body = document.body;
 var section = '<section></section>';
 
 function makeMiddleEarth() {
-  // your answers here
+    // create a section tag with an id of `middle-earth`
+   // add each land as an `article` tag
+   // inside each `article` tag include an `h1` with the name of the land
+   // append `middle-earth` to your document `body`
+    const sxn = $('<section>');
+    sxn.attr('id', 'middle-earth');
+    for(i = 0; i < lands.length; i++){
+      const article = $('<article>')
+      sxn.append(article);
+      const h1 = $('<h1>').text(lands[i])
+      article.append(h1)
+    }
+  $('body').append(sxn)
 }
 
 makeMiddleEarth();
 
 function makeHobbits(){
-  // your answers here
+     // display an `unordered list` of hobbits in the shire
+    // (which is the second article tag on the page)
+   // give each hobbit a class of `hobbit`
 }
 
 makeHobbits();
