f037825eb3cd406d4d673e7434c16009e4479031
Samat Baltin <somych1@gmail.com>
2018-03-22 22:05:49 -0500

    Done homework6

--- scripts/fellowship.js
+++ scripts/fellowship.js
@@ -20,74 +20,125 @@ var lands = ['The Shire', 'Rivendell', 'Mordor'];
 var body = document.body;
 var section = '<section></section>';
 
-function makeMiddleEarth() {
-  // your answers here
+function makeMiddleEarth(){
+  const newSection = $('<section id="middle-earth">');
+  for(let i = 0; i < lands.length; i++){
+    const article = $('<article>');
+    newSection.append(article);
+    const h1 = $('<h1>').text(lands[i]);
+    article.append(h1);
+  }
+  $('body').append(newSection);
 }
 
 makeMiddleEarth();
 
+
+
 function makeHobbits(){
-  // your answers here
+  const unList = $('<ul>');
+  for(let i = 0; i < hobbits.length; i++){
+    const listIt = $('<li class="hobbit">').text(hobbits[i]);
+
+    unList.append(listIt);
+  }
+  $('article').eq(0).append(unList)
 }
 
 makeHobbits();
 
 function keepItSecretKeepItSafe(){
-  // your answers here
+  const ring = $('<div id="the-ring" class="magic-imbued-jewelry">');
+  console.log(ring)
+  $('.hobbit').eq(0).append(ring)
 }
 
 keepItSecretKeepItSafe();
 
 function makeBuddies(){
-  // your answers here
+  const aside = $('<aside>')
+  const unList = $('<ul>')
+  aside.append(unList)
+  for(let i = 0; i < buddies.length; i++){
+    const listIt = $('<li>').text(buddies[i])
+    unList.append(listIt)
+  }
+  $('article').eq(1).append(aside)
 }
 
 makeBuddies();
 
 function beautifulStranger(){
-  // your answers here
+  $('li').eq(7).text('Aragorn')
 }
 
 beautifulStranger();
 
 function leaveTheShire(){
-  // your answers here
+  $('ul').eq(0).appendTo('aside');
 }
 
 leaveTheShire();
 
 function forgeTheFellowship() {
-  // your answers here
+  const div = $('<div id="the-fellowship">');
+  $('article').eq(1).append(div)
+  const unList = $('<ul>')
+  unList.appendTo(div)
+  for(let i = 0; i < $('li').length; i++){
+    console.log($('li').eq(i).text() + ' has joined your party')
+    $('li').eq(i).appendTo(unList)
+  }
 }
 
 forgeTheFellowship();
 
+$('.overlay').css({
+  'background-color': 'rgba(0,0,0,0.0)'
+})
+
 function theBalrog(){
-  // your answers here
+  $('li').eq(4).text('Gandalf the White')
+  $('li').eq(4).css({
+    'background-color': 'white',
+    'border-color': 'grey',
+  })
+  // apply the following style to the element, make the // background 'white', add a grey border
 }
 
 theBalrog();
 
 function hornOfGondor() {
-  // your answers here
+  alert('The horn of gondor has been blown')
+  console.log('The horn of gondor has been blown')
+  alert("Boromir's been killed by the Uruk-hai!")
+  console.log("Boromir's been killed by the Uruk-hai!")
+  $('li').eq(5).remove()
 }
 
 hornOfGondor();
 
 function itsDangerousToGoAlone() {
-  // your answers here
+  $('li').eq(2).appendTo($('article').eq(2))
+  $('li').eq(6).appendTo($('article').eq(2))
+  const div = $('<div id="mount-doom">');
+  div.appendTo($('article').eq(2))
 }
 
 itsDangerousToGoAlone();
 
 function weWantsIt() {
-  // your answers here
+  const div = $('<div id="gollum">Gollum</div>')
+  div.appendTo($('article').eq(2))
+  $('div').eq(2).appendTo(div)
+  div.appendTo($('div').eq(2))
 }
 
 weWantsIt();
 
 function thereAndBackAgain(){
-  // your answers here
+  $('div').eq(3).remove();
+  $('.hobbit').appendTo($('article').eq(0))
 }
 
 thereAndBackAgain();
