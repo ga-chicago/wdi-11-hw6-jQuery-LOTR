12df1d87d54d814888580764225472a47fd36127
Noemi Romero <nlll02@yahoo.com>
2018-03-22 23:08:09 -0500

    create an aside tag

--- scripts/fellowship.js
+++ scripts/fellowship.js
@@ -1,4 +1,4 @@
-console.log("Linked.");
+console.log("Linked.asdfvasdfasdf");
 
 // Dramatis Personae
 var hobbits = [
@@ -20,32 +20,73 @@ var lands = ['The Shire', 'Rivendell', 'Mordor'];
 var body = document.body;
 var section = '<section></section>';
 
-function makeMiddleEarth() {
-  // your answers here
+
+function makeMiddleEarth() { console.log('hey')
+  const syx = $('<section>').attr('id', 'middle-earth')
+  for(let i =0; i< lands.length; i++){
+    
+    const article = $('<article>')
+    syx.append(article);
+
+    const h1 = $('<h1>').text(lands[i])
+    article.append(h1)
+
+  }
+  $('body').append(syx)
 }
 
 makeMiddleEarth();
 
 function makeHobbits(){
-  // your answers here
+  /// display an `unordered list` of hobbits in the shire
+
+  const ul = $('<ul>').addClass('hobbit')
+  ul.appendTo($('article').eq(0))
+      // (which is the second article tag on the page)
+  for(let i =0; i < hobbits.length; i++){
+    const li = $('<li>').text(hobbits[i])
+    $(ul).append(li)
+  }
+   // give each hobbit a class of `hobbit`
 }
+  
+  
 
 makeHobbits();
 
 function keepItSecretKeepItSafe(){
-  // your answers here
+  // create a div with an id of `'the-ring'`
+  const newDiv = $('<div>').attr('id', 'the-ring').addClass('magic-imbued-jewelry');
+  
+   // give the div a class of `'magic-imbued-jewelry'`
+    //('newDiv').appendTo($('li'))
+   // add the ring as a child of `Frodo`
 }
 
 keepItSecretKeepItSafe();
 
 function makeBuddies(){
-  // your answers here
-}
+  // create an `aside` tag
+  
+   const aside = $('<aside>')
+   // attach an `unordered list` of the `'buddies'` in the aside
+    aside.appendTo($('article').eq(1))
+   // insert your aside as a child element of `rivendell
+    
+    const ul = $('<ul>').addClass('buddies')
+    $(aside).append(ul)
+    for(let i =0; i < buddies.length; i++){
+    const li = $('<li>').text(buddies[i])
+    $(ul).append(li)
+  
 
+  }
+}
 makeBuddies();
 
 function beautifulStranger(){
-  // your answers here
+// change the `'Strider'` text to `'Aragorn'`
+ul.buddies.text('Aragon')
 }
 
 beautifulStranger();
@@ -91,3 +132,5 @@ function thereAndBackAgain(){
 }
 
 thereAndBackAgain();
+
+
